a75969f8f208aaa8ed8a762648f8054d19eecf5a
MJackson <jackswalter7@gmail.com>
2020-10-08 08:29:00 +0200

    Review: adding settings to .env file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import routes from './src/routes/routes';
 import swaggerDocument from './swagger.json';
+import {} from 'dotenv/config'
 import db from './src/config/connection.js'
 
 const app = express();
@@ -17,7 +18,7 @@ db.authenticate()
     .then(() => console.log("Database connected..."))
     .catch(err => console.log('Error: ' + err))
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port} ...`);
--- src/config/connection.js
+++ src/config/connection.js
@@ -2,12 +2,14 @@ import Sequelize from 'sequelize'
 // import pg from 'pg'
 
 // pg.defaults.ssl = true;
-const DB_NAME = 'ddibi6hgtgr7ol'
-const USERNAME = 'jqyhwdhfxulrub'
-const PASSWORD = '1e7ff0a83ebe2d9ac0af18133f9b949412391fb4b4440f833cd309de437ea52a'
-const HOST = 'ec2-54-160-18-230.compute-1.amazonaws.com'
+const DB_NAME = process.env.DB_NAME
+const DB_USERNAME = process.env.DB_USERNAME
+const PASSWORD = process.env.PASSWORD
+const HOST = process.env.HOST
+console.log(process.env.DB_NAME);
 
-const db = new Sequelize(DB_NAME, USERNAME, PASSWORD, {
+
+const db = new Sequelize(DB_NAME, DB_USERNAME, PASSWORD, {
     host: HOST,
     port: 5432,
     dialect: 'postgres',
