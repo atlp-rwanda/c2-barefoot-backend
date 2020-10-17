6ab92cd3f05cb4794c9c157fdc09870d94b4210c
inezabonte <inezabonte@gmail.com>
2020-10-17 22:35:00 +0200

    created seeds and migrations for signup page

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import swaggerUI from 'swagger-ui-express';
 import bodyParser from 'body-parser';
 import routes from './routes/routes';
 import swaggerDocument from '../swagger.json';
+import db from './config/connection';
 
 const app = express();
 
@@ -15,6 +16,10 @@ app.use('/', routes);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
+
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch((err) => console.log(`Error: ${err}`));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port} ...`);
