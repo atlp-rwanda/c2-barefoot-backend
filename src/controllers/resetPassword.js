26a3f8ba8f1ba8a277154a5a2dea2cbfa7e81bc0
lyndaflower <umurerwalynda@gmail.com>
2020-10-28 07:01:53 +0200

    add invalidate token functions

--- src/controllers/resetPassword.js
+++ src/controllers/resetPassword.js
@@ -1,7 +1,7 @@
 import { findUserByEmail, updateUserPassword } from "../services/userServices";
 import models from "../models";
 import jwt from "jsonwebtoken";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import { config } from "dotenv";
 import { validateResetEmail } from "../validations/userValidation";
 import sendResetPasswordLink from "../controllers/sendEmail";
@@ -15,25 +15,36 @@ export const resetPassword = async (req, res) => {
     return res.status(404).json({ status: 404, error: "User not found" });
   if (!userFound.verified)
     return res.status(401).json({ status: 401, error: "Account not verified" });
-  const token = jwt.sign({ email: userFound.email }, process.env.TOKEN_SECRET, {expiresIn: 300});
+  const token = jwt.sign({ email: userFound.email }, process.env.TOKEN_SECRET, {
+    expiresIn: 300,
+  });
   sendResetPasswordLink(userFound.email);
   return res
     .status(200)
     .json({ status: 200, message: "Request sent successfully", data: token });
 };
 
- 
-
-export const verifyResetPassword = async(req, res) => {
+export const verifyResetPassword = async (req, res) => {
   const { token } = req.query;
   const { password, confirmPassword } = req.body;
-  
-  const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-  console.log(decodedToken)
 
-  if (!decodedToken) return res.status(403).json({ status: 403, error: 'INvalid TOken' });
-  if (password === confirmPassword) return res.status(400).json({ status: 400, error: 'Passwords do not match' });
+  const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+  console.log(decodedToken);
+
+  if (!decodedToken)
+    return res.status(403).json({ status: 403, error: "INvalid TOken" });
+  if (password === confirmPassword)
+    return res
+      .status(400)
+      .json({ status: 400, error: "Passwords do not match" });
   const hashedPassword = bcrypt.hashSync(password, 10);
-  const updatedPassword = await updateUserPassword(decodedToken.user, hashedPassword);
-  return res.status(200).json({ status: 200, message: 'Password reset successful', data: updatedPassword})
-}
+  const updatedPassword = await updateUserPassword(
+    decodedToken.user,
+    hashedPassword
+  );
+  return res.status(200).json({
+    status: 200,
+    message: "Password reset successful",
+    data: updatedPassword,
+  });
+};
--- src/controllers/sendEmail.js
+++ src/controllers/sendEmail.js
@@ -1,15 +1,15 @@
-import nodemailer from 'nodemailer';
-import jwt from 'jsonwebtoken';
-import 'dotenv/config';
+import nodemailer from "nodemailer";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const sender = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 465,
   secure: true, // use SSL
   auth: {
     user: process.env.GMAIL_EMAIL,
-    pass: process.env.GMAIL_PASSWORD
-  }
+    pass: process.env.GMAIL_PASSWORD,
+  },
 });
 
 const sendResetPasswordLink = async (data) => {
@@ -17,9 +17,8 @@ const sendResetPasswordLink = async (data) => {
   const mailOptions = {
     from: `"Barefoot Nomad"<${process.env.GMAIL_EMAIL}>`,
     to: data,
-    subject: 'Reset password',
-    html:`<p>Welcome to barefoot nomad, click on this <a href=http://localhost:3000/resetPassword/verify?token=${accessToken}>link</a> to reset your password</p>`
-
+    subject: "Reset password",
+    html: `<p>Welcome to barefoot nomad, click on this <a href=http://localhost:3000/resetPassword/verify?token=${accessToken}>link</a> to reset your password</p>`,
   };
   const sendEmail = await sender.sendMail(mailOptions);
   console.log(sendEmail);
