230cbc7d89f3c737022e2fdbe34880af6ab32b44
habaumugisha1 <habajeunes2@gmail.com>
2020-11-21 16:07:15 +0200

    adding notification seeds

--- src/seeders/20201119180537-notification.js
+++ src/seeders/20201119180537-notification.js
@@ -1,12 +1,12 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Notifications', {
-      id: 'fb94de4d-47ff-4079-89e8-b0186c0a3br4',
+    await queryInterface.bulkInsert('Notifications', [{
+      id: '941e130e-02f8-46d9-9bef-9b3e50feba2a',
       userId: 'fb94de4d-47ff-4079-89e8-b0186c0a3be8',
       title: 'approve',
       message: 'your travel request was approved',
       status: 'unreaded'
-    });
+    }]);
   },
 
   down: async (queryInterface, Sequelize) => {
