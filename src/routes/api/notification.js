00ba5c1a7f80fa9781fd3ab011bccee7e97c242b
habaumugisha1 <habajeunes2@gmail.com>
2020-11-24 09:59:29 +0200

    adding documentation for end points

--- src/routes/api/notification.js
+++ src/routes/api/notification.js
@@ -4,6 +4,68 @@ import updateNotification from '../../controllers/notification/updateNotificatio
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/notification/notifications:
+ *   get:
+ *     tags:
+ *       - Get user's Notifcations
+ *     summary: When user is loged in should get all notification related to him/her
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: success data
+ *         schema:
+ *           data:
+ *           type: object
+ *           properties:
+ *             status:
+ *               type: integer
+ *               description: The http status code
+ *               example: 200
+ *             message:
+ *               type: string
+ *               description: success message
+ *               example: verified users
+ *             notifications:
+ *               type: object
+ *               description: retrieved notifications
+ *
+ */
 router.get('/notifications', notifications);
+
+/**
+ * @swagger
+ * /api/v1/notification/notifications/:id:
+ *   patch:
+ *     tags:
+ *       - Update notification
+ *     summary: it requere to click on notification for updated
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: success data
+ *         schema:
+ *           data:
+ *           type: object
+ *           properties:
+ *             status:
+ *               type: integer
+ *               description: The http status code
+ *               example: 200
+ *             message:
+ *               type: string
+ *               description: success message
+ *               example: Notification upadted successful
+ *
+ */
 router.patch('/notifications/:id', updateNotification);
 export default router;
