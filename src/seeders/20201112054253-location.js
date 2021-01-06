371536e3ab65874b9af62ce5b584602b9ea286cc
MJackson <jackswalter7@gmail.com>
2021-01-06 09:02:28 +0200

    Adding Image field to locations (#44)

--- src/seeders/20201112054253-location.js
+++ src/seeders/20201112054253-location.js
@@ -3,6 +3,7 @@ export default {
     {
       id: 'c6028e0d-ef88-4693-ab49-f37669891724',
       LocationName: 'Kigali',
+      LocationImage: 'https://res.cloudinary.com/barefoot-nomad-app/image/upload/v1609873865/images/western-capelist-of-schools-database_d7nvou.jpg',
       country: 'Rwanda',
       description: 'The land of a thousand hills',
       link: 'https://en.wikipedia.org/wiki/Kigali'
