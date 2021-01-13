a51a7631c21a72b5da593643c79171b1c6519a65
Ineza Bonté Grévy <inezabonte@gmail.com>
2021-01-13 21:44:55 +0200

    Fix managers routes (#51)
    * allow administrator to access manager routes
    
    * removed console
    
    * add role and line manager to retrieved info

--- src/services/findManager.js
+++ src/services/findManager.js
@@ -1,10 +1,8 @@
 import models from '../models';
 
-const manager = async (data, page = 1) => {
-  const pageSize = 12;
-  const skip = parseInt((page - 1) * pageSize, 10);
+const manager = async (data) => {
   const verifiedUserManager = await models.User.findAndCountAll({
-    offset: skip, limit: pageSize, where: { verified: true, user_role_id: data }, attributes: { exclude: ['password', 'refreshtoken'] }, required: false
+    where: { verified: true, user_role_id: data }, attributes: { exclude: ['password', 'refreshtoken'] }, required: false
   });
   return verifiedUserManager;
 };
